5a3f7277c0f5066aed1db9410967f9b749940d87
NIGHT LIGHT <jaredinyaagha@gmail.com>
2021-11-25 09:58:24 -0500

    added local storage button list creator

--- script.js
+++ script.js
@@ -3,15 +3,18 @@ var cityUrl = "api.openweathermap.org/data/2.5/weather?q="
 
 var searchBtn = $('#searchBtn');
 var searchBar = $('#searchBar');
+var historyBlocks = $('#historyBlocks')
 
 $(document).ready();
 
 function searchCity() {
     var input = searchBar.val();
-
+    inputVal = JSON.stringify(input);
+    localStorage.setItem(input, inputVal);
     if (input) {
         // console.log(input)
-        fetchCity(encodeURI(input));
+        // fetchCity(encodeURI(input));
+        createHistoryBlocks(input);
     }
 }
 
@@ -29,6 +32,29 @@ function fetchCity(input) {
 
 }
 
+function createToday() {}
+
+function createForecast() {}
+
+function createHistoryBlocks(input) {
+    var input = searchBar.val();
+    var cityHistory = JSON.parse(localStorage.getItem(input));
+    console.log(cityHistory);
+
+    if (input) {
+        var cityEl = document.createElement("BUTTON");
+        var historyNode = document.createTextNode(cityHistory);
+        cityEl.appendChild(historyNode);
+        cityEl.classList.add("p-2", "btn", "btn-primary", "bg-secondary", "border-secondary");
+        historyBlocks.append(cityEl);
+        
+    }
+
+    else {
+        console.log("error");
+    };
+        
+}
 
 searchBtn.on("click", searchCity);
 searchBar.on("keydown", function(event) {
