4427f956fac257a2d588c00aa379867cee05b90b
NIGHT LIGHT <jaredinyaagha@gmail.com>
2021-11-28 19:46:46 -0500

    redid LS button constructor

--- script.js
+++ script.js
@@ -1,28 +1,26 @@
 var APIKey = "3be42a9dae422509e4fc0f4a269e4f20";
 var cityUrl = "https://api.openweathermap.org/data/2.5/weather?q=" 
 
-var searchBtn = $('#searchBtn');
-var searchBar = $('#searchBar');
-var historyBlocks = $('#historyBlocks');
-var today = $('#today');
-var temp = $('#temp');
-var wind = $('wind');
-var humidity = $('humidity');
-var uvIndex = $('uvIndex');
+var searchBtn = $("#searchBtn");
+var searchBar = $("#searchBar");
+var historyButtons = $("#historyBlocks");
+var rightCol = $("#rightCol");
+// var today = $('#today');
+// var temp = $('#temp');
+// var wind = $('wind');
+// var humidity = $('humidity');
+// var uvIndex = $('uvIndex');
 
-// function TodayBlock(todayName, todayTemp, todayWind, todayHum, todayUV) {
-//     this.
-// }
 $(document).ready();
 
 function searchCity() {
     var input = searchBar.val();
     inputVal = JSON.stringify(input);
-    localStorage.setItem(input, inputVal);
+    
     if (input) {
-        // console.log(input)
-        fetchCity(encodeURI(input));
-        createHistoryBlocks(input);
+        localStorage.setItem(input, inputVal);        
+        // fetchCity(encodeURI(input));        
+        allStorage();
     }
 }
 
@@ -40,33 +38,61 @@ function fetchCity(input) {
         .then(function (data) {
                 console.log(data)
                 var currentTemp = data.main.temp;
-                var kelvToFahr = Math.round((currentTemp - 273.15) * 9/5 + 32)
-                console.log(kelvToFahr)
+                var kelvToFahr = Math.round((currentTemp - 273.15) * 9/5 + 32);
+                console.log(kelvToFahr);
+                createTodayBlock();
             })
 }
 
-function createTodayBlock () {}
+function createTodayBlock() {
+    rightCol.classList.toggle("visually-hidden");
+}
 
 function createForecastBlocks() {}
 
-function createHistoryBlocks(input) {
-    var input = searchBar.val();
-    var cityHistory = JSON.parse(localStorage.getItem(input));
+function allStorage() {
+    var searchedCities = [],
+        keys = Object.keys(localStorage),
+        i = keys.length;
     
-    if (input) {
-        var cityEl = document.createElement("BUTTON");
-        var historyNode = document.createTextNode(cityHistory);
-        cityEl.appendChild(historyNode);
-        cityEl.classList.add("p-2", "btn", "btn-primary", "bg-secondary", "border-secondary");
-        historyBlocks.append(cityEl);
+    while (i--) {
+        cityName = JSON.parse(localStorage.getItem(keys[i]));
+        searchedCities.push(cityName);
         
     }
-
-    else {
-        console.log("error");
-    };
-        
+    console.log(searchedCities);
+    createHistoryButtons(searchedCities)
 }
+
+function createHistoryButtons(input) {
+    var input;
+    Object.keys(input).forEach((key, index) => {
+        var buttonText = input[index];
+        var button = document.createElement("button");
+        button.className = "p-2 btn btn-primary bg-secondary border-secondary";
+        button.textContent = buttonText;
+        historyButtons.append(button)
+
+
+    })
+    }
+    // var input = searchBar.val();
+    // var cityHistory = JSON.parse(localStorage.getItem(input));
+    
+    // if (input) {
+    //     var cityEl = document.createElement("BUTTON");
+    //     var historyNode = document.createTextNode(cityHistory);
+    //     cityEl.appendChild(historyNode);
+    //     cityEl.classList.add("p-2", "btn", "btn-primary", "bg-secondary", "border-secondary");
+    //     historyBlocks.append(cityEl);
+        
+    // }
+
+    // else {
+    //     console.log("error");
+    // };
+        
+// }
 
 searchBtn.on("click", searchCity);
 // searchBar.on("keydown", function(event) {
