0457a18b9138b4fbc422de60e5f44324e8130086
NIGHT LIGHT <jaredinyaagha@gmail.com>
2022-01-02 10:15:34 -0500

    replaced files

---
+++ README.md
@@ -0,0 +1,8 @@
+# Weather-Dashboard
+
+# Description
+
+This webpage is a simple Weather Dashboard application that I created using jQuery and an API service called OpenWeatherMap. One of the main reasons why I created this application was to practice making API calls and utilizing the data that returned in a variety of ways. In this project I worked closely with moment js again as well as the localstorage feature of the google chrome web browser.
+
+![An Image of my Weather Dashboard]()
+
--- script.js
+++ script.js
@@ -1,33 +1,56 @@
 var APIKey = "3be42a9dae422509e4fc0f4a269e4f20";
-var cityUrl = "https://api.openweathermap.org/data/2.5/weather?q=" 
+var coordUrl = "https://api.openweathermap.org/data/2.5/weather?q=" 
+var cityInfoUrl = "https://api.openweathermap.org/data/2.5/onecall?lat="
 
 var searchBtn = $("#searchBtn");
 var searchBar = $("#searchBar");
 var historyButtons = $("#historyBlocks");
 var rightCol = $("#rightCol");
-// var today = $('#today');
-// var temp = $('#temp');
-// var wind = $('wind');
-// var humidity = $('humidity');
-// var uvIndex = $('uvIndex');
 
-$(document).ready();
+var nameDate = $('#nameDate');
+var icon0 = $('#icon0');
+var temp0 = $('#temp0');
+var wind0 = $('#wind0');
+var humidity0 = $('#humidity0');
+var uvIndex0 = $('#uvIndex0');
 
+var fiveDayList = $('.fiveDay')
+var histBtn = $('.histBtn');
+var date0 = moment().format('L');
+
+$(document).ready(function() {
+//////SEARCH HANDLING
 function searchCity() {
-    var input = searchBar.val();
-    inputVal = JSON.stringify(input);
-    
-    if (input) {
-        localStorage.setItem(input, inputVal);        
-        fetchCity(encodeURI(input));        
-        cityStorage();
-        // createTodayBlock();
+    initInput = searchBar.val(),
+    inputVal = JSON.stringify(initInput);    
+    if (initInput) {
+        localStorage.setItem(initInput, inputVal);        
+        fetchCoords(encodeURI(initInput));        
+        storagetoButtons();
+        
     }
 }
 
-function fetchCity(input) {
-    var requestUrl = cityUrl + input + "&appid=" + APIKey;
-    // console.log(requestUrl);
+// function handleSearchHistory(event) {
+//     var button = event.target;
+//     var city = button.getAttribute("value");
+//     console.log(city);
+//     fetchCoords(encodeURI(city));
+// }
+// 
+// function searchHistory() {
+//     var input = histBtn.val();
+//     console.log(input);
+//     if (input) {
+//         fetchCoords(encodeURI(input));
+//     }
+//
+// }
+//////SEARCH HANDLING
+
+//////API DATA CALL
+function fetchCoords(input) {
+    var requestUrl = coordUrl + input + "&appid=" + APIKey;
     fetch(requestUrl)
         .then(function(response) {            
             if (!response.ok) {
@@ -37,22 +60,86 @@ function fetchCity(input) {
                 return response.json();
             })        
         .then(function (data) {
-                console.log(data);
-                var currentTemp = data.main.temp;
-                var kelvToFahr = Math.round((currentTemp - 273.15) * 9/5 + 32);
-                // console.log(kelvToFahr);
-                // createTodayBlock(kelvToFahr, data.main.)
+                // console.log(data);                
+                var coordUrl = cityInfoUrl + data.coord.lat + "&lon=" + data.coord.lon + "&exclude=hourly,minutely&appid=" + APIKey;
+                dataName = data.name;
+                // console.log(coordUrl);
+                fetchCityInfo(coordUrl);
             })
         
 }
 
-function createTodayBlock() {
-    rightCol.removeClass("d-none");
+function fetchCityInfo(input) {
+    fetch(input)
+        .then(function(response) {
+            if (!response.ok) {
+                throw response.json();
+                }
+
+                return response.json();
+        })
+        .then(function (data) {
+
+                var fiveDayData = [data.daily];
+                createBlocks(fiveDayData[0]);
+        })
 }
+//////API CALL
 
-function createForecastBlocks() {}
+//////BLOCK CREATION
+function createBlocks(input) {
+    rightCol.removeClass("d-none");
+    
+    var i = 0;
+    for (var count = 0; count < 2; count++) {
+        if (i == 0) {
+            var iconcode = input[0].weather[0].icon,
+            iconURL = "http://openweathermap.org/img/w/" + iconcode + ".png";
+            icon0.attr('src', iconURL);
+            nameDate.html(JSON.parse(localStorage.getItem(initInput)));
+            nameDate.append(" (" + date0 + ")");
+            var currentTemp = Math.round((input[0].temp.day - 273.15) * 9/5 + 32);
+            temp0.html("Temp: " + currentTemp + "°F");
+            wind0.html("Wind: " + input[0].wind_speed + " MPH");
+            humidity0.html("Humidity: " + input[0].humidity + " %");
+            uvIndex0.html("UV Index: " + input[0].uvi);
+            i = 1;
+        }
 
-function cityStorage() {
+        else if (i == 1) {
+            
+            var j = 0;
+            
+            fiveDayList.each(function(index) {                
+                j++;
+                // console.log($(this).attr("id"));
+                var thisId = $(this).attr("id"),
+                newDate = moment().add(thisId, 'days').format('L'),
+                iconcode = input[thisId].weather[0].icon,
+                iconURL = "http://openweathermap.org/img/w/" + iconcode + ".png",
+                thisIcon = $(this).children('li.icon').children('img'),
+                thisTemp = $(this).children('li.temp'),
+                currentTemp = Math.round((input[thisId].temp.day - 273.15) * 9/5 + 32),
+                thisWind = $(this).children('li.wind'),
+                thisHumidity = $(this).children('li.humidity');
+                thisDate = $(this).children('li.date')
+                
+                thisDate.html(" (" + newDate + ")");
+                thisIcon.attr('src', iconURL);
+                thisTemp.html("Temp: " + currentTemp + "°F");
+                thisWind.html("Wind: " + input[thisId].wind_speed + " MPH");
+                thisHumidity.html("Humidity: " + input[thisId].humidity + " %")
+
+                // console.log(thisId);
+            });
+        }
+    }      
+  
+}
+//////BLOCK CREATION
+
+//////BUTTON CREATION
+function storagetoButtons() {
     var searchedCities = [],
         keys = Object.keys(localStorage),
         i = keys.length;
@@ -62,44 +149,37 @@ function cityStorage() {
         searchedCities.push(cityName);
         
     }
-    // console.log(searchedCities);
-    createHistoryButtons(searchedCities)
+    createHistoryButtons(searchedCities)    
 }
 
 function createHistoryButtons(input) {
     var input;
-    Object.keys(input).forEach((key, index) => {
+    historyButtons.empty();
+    input.forEach((key, index) => {
         var buttonText = input[index];
         var button = document.createElement("button");
-        button.className = "p-2 btn btn-primary bg-secondary border-secondary";
-        button.textContent = buttonText;
-        historyButtons.append(button)
-
-
+        button.className = "p-2 btn btn-primary bg-secondary border-secondary histBtn";
+        button.setAttribute("value", buttonText);
+        button.innerHTML = buttonText;
+        historyButtons.append(button);
     })
-    }
-    // var input = searchBar.val();
-    // var cityHistory = JSON.parse(localStorage.getItem(input));
     
-    // if (input) {
-    //     var cityEl = document.createElement("BUTTON");
-    //     var historyNode = document.createTextNode(cityHistory);
-    //     cityEl.appendChild(historyNode);
-    //     cityEl.classList.add("p-2", "btn", "btn-primary", "bg-secondary", "border-secondary");
-    //     historyBlocks.append(cityEl);
-        
-    // }
+}
+//////BUTTON CREATION
 
-    // else {
-    //     console.log("error");
-    // };
-        
-// }
-
+//////EVENT HANDLERS
 searchBtn.on("click", searchCity);
+histBtn.on("click", function(event) {
+    handleSearchHistory(event);
+})
 // searchBar.on("keydown", function(event) {
 //     if(event.key === 'Enter') {
 //         searchCity;
 //     }
 //     else{console.log("ayy")}
 // });
+   
+//////EVENT HANDLERS
+});
+
+
